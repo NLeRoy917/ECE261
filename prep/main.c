90ac76cb0f73a431debcea8bfb8324f72fadb3fe
Nathan LeRoy <NLeRoy917@gmail.com>
2020-07-02 18:12:49 +0000

    add july 2nd sessions files

--- prep/main.c
+++ prep/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World\n");
-  return 0;
+    int num, originalNum, remainder, result = 0;
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+
+    printf("Hello, Abdullah can see me editing the file.\n");
+
+    while (originalNum != 0) {
+       // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       // removing last digit from the orignal number
+       originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
 }

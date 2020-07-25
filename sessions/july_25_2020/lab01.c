83cd38f8e5ca279eae73d6f58d49e9b22148d41d
Nathan LeRoy <NLeRoy917@gmail.com>
2020-07-25 18:40:27 +0000

    more changes

---
+++ sessions/july_25_2020/lab01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 1.	Prompt the user to enter the first integer number. 
+
+  // 2.	Store the user input in a variable
+
+  // 3.	Display the number the user enters.
+
+  // 4.	Repeat the above three procedures for the second number.
+
+  // 5.	Add the two numbers and store the result in a third variable.
+
+  // 6.	Display the result in the console window.
+
+  return 0;
+}
+
